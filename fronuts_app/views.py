669147bed080be15e1321742cc29a69fd0e88eb8
dashnash <dashiell.nash@gmail.com>
2014-11-02 22:15:57 -0800

    Adding landing page
    Added fronut list

--- fronuts_app/views.py
+++ fronuts_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.views import generic
+
+from fronuts_app.models import User, Shop, Fronut
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'fronuts_app/index.html'
+    context_object_name = 'fronuts_list'
+    
+    def get_queryset(self):
+        return Fronut.objects.order_by('-date')
